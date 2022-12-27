8026eece52d9094aa9de258e3aafbb6e76dc5a88
u-010420 <u010420.developer@gmail.com>
2022-12-27 19:50:53 +0000

    Docs: Added Comments + Reformatting

--- next-tsx/components/footer.tsx
+++ next-tsx/components/footer.tsx
@@ -1,3 +1,4 @@
+/* @Footer • Dynamic Component */
 import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
 
--- next-tsx/components/layout.tsx
+++ next-tsx/components/layout.tsx
@@ -1,3 +1,4 @@
+/* @Layout [GLOBAL] */
 import Head from 'next/head';
 //import Link from 'next/link';
 //import Image from 'next/image';
