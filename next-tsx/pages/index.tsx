1ed89dbce33fa0e2cbb847a6e3c08ffc24c1461b
u-010420 <u010420.developer@gmail.com>
2022-12-27 02:39:36 +0000

    Build: Home Page Design

--- next-tsx/pages/index.tsx
+++ next-tsx/pages/index.tsx
@@ -13,7 +13,18 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <Layout>
+        <div className="container-100vh">
+          <div className="center-xy">
 
+            <small className="fw-bold">0P3NS0URCE PR0J3CT5</small>
+            <h1 className="mb-4">SOFTWAREDEV</h1>
+            <a className="btn btn-outline-light">Read More</a>
+            <div className="d-flex justify-content-end mt-5 fw-bold">
+              <small>&#x1F4E6; Version 1.0.0</small>
+            </div>
+
+          </div>
+        </div>
       </Layout>
     </>
   );
